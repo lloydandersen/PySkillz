2311b8d03611dad6f21e448bb60c0949c7b642f9
lloydandersen <lloydandersenco@gmail.com>
2024-07-27 22:31:40 -0700

    I began a skill building app.

---
+++ main/quiz.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.minsize(600, 600)
+root.title("PySkillz")
+
+answer_one_var = tk.StringVar()
+answer_two_var = tk.StringVar()
+answer_three_var = tk.StringVar()
+answer_four_var = tk.StringVar()
+answer_five_var = tk.StringVar()
+correct_answer_var = tk.StringVar()
+question_var = tk.StringVar()
+
+
+
+def run_quiz():
+    pass
+
+root.mainloop()
